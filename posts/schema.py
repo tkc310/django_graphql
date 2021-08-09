29828643b5b6859dc865e1b0178082d696d9a64e
Takashi Sato <takashisato.235711@gmail.com>
2021-08-09 17:54:00 +0900

    postByIdのスキーマを追加

--- posts/schema.py
+++ posts/schema.py
@@ -9,9 +9,14 @@ class Post(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     posts = graphene.List(Post)
+    post = graphene.Field(Post, id=graphene.Int())
 
     def resolve_posts(self, info, **kwargs):
         return PostModel.objects.all()
+
+    def resolve_post(self, info, **kwargs):
+        id = kwargs.get('id')
+        return PostModel.objects.get(pk=id)
 
 class CreatePost(graphene.Mutation):
     post = graphene.Field(lambda :Post)
